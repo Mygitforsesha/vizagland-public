c4d27ac647eac768ed4c694366487a2713803b7f
Mygitforsesha <ksesha021@gmail.com>
2026-06-07 16:04:43 +0530

    update multi select design

--- src/pages/PostPropertyPage.jsx
+++ src/pages/PostPropertyPage.jsx
@@ -377,7 +377,7 @@ export function PostPropertyPage() {
               </h3>
             </div>
 
-            <div className="p-4 sm:p-5 md:p-6 space-y-2">
+          <div className="p-4 sm:p-5 md:p-6 grid grid-cols-1 md:grid-cols-2 gap-6">
            
 
               
